0d52b8e24b0682304411ecb2a6cc73062c72e25a
Burak Can Altunoglu <burakcanaltunoglu@gmail.com>
2023-08-08 08:44:24 +0300

    added-NodeJS

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false }));
+
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers",
+    res.setHeader(
+     "Access-Control-Allow-Headers",
      "Origin, X-Requested-With, Content-Type, Accept"
      );
      res.setHeader("Access-Control-Allow-Methods",
@@ -12,6 +17,15 @@ app.use((req,res,next) => {
      );
     next();
 });
+
+app.post("/api/posts",(req,res,next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added successfully'
+    });
+});
+
 
 app.use('/api/posts',(req,res,next) => {
     const posts = [
@@ -28,7 +42,7 @@ app.use('/api/posts',(req,res,next) => {
 
     ];
     res.status(200).json({
-        message: 'Posts fetched succesfully',
+        message: 'Posts fetched successfully',
         posts: posts
     });
 });
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -26,8 +26,13 @@ import { Post } from "./post.model";
 
     addPost(title: string, content: string){
         const post: Post = {id: null, title: title, content: content};
-        this.posts.push(post); 
-        this.postsUpdated.next([...this.posts]);
+        this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+            .subscribe((responseData) => {
+                console.log(responseData.message);
+                this.posts.push(post); 
+                this.postsUpdated.next([...this.posts]);
+            });
+
     }
     
  }
